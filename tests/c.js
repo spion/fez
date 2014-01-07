bf979d5e7f14399d1aa63c1292fd5839f897eb00
Isaac Wagner <isaacbwagner@gmail.com>
2014-01-07 16:57:44 -0500

    Added first test, based on the c example project

---
+++ tests/c.js
@@ -0,0 +1,32 @@
+var test = require("tape"), 
+    exec = require("child_process").exec,
+    fs = require("fs");
+
+test('c project', function(t) {
+  t.plan(4);
+
+  setup();
+
+  function setup() {
+    var child = exec("cd tests/c/; node fez.js");
+    child.on("exit", function(code) {
+      t.equal(code, 0);
+      stat();
+    });
+  }
+
+  function stat() {
+    t.ok(fs.existsSync('tests/c/hello.o'));
+    t.ok(fs.existsSync('tests/c/square.o'));
+    t.ok(fs.existsSync('tests/c/hello'));
+
+    teardown();
+  }
+
+  function teardown() {
+    exec("rm tests/c/hello tests/c/*.o", function(err) {
+      if(err) t.fail(err.message);
+      else t.end();
+    });
+  }
+});
