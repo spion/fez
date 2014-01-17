bfc0a1a2bf641bb4133c9c5f03a8fb763db1a7f2
Isaac Wagner <isaacbwagner@gmail.com>
2014-01-16 19:42:07 -0500

    Use concat instead of clean to concat

--- examples/less/fez.js
+++ examples/less/fez.js
@@ -1,9 +1,10 @@
 var fez = require("../../src/main.js"),
     less = require("fez-less"),
-    clean = require("fez-clean-css");
+    clean = require("fez-clean-css"),
+    concat = require("fez-concat");
 
 exports.build = function(rule) {
-  rule("dist/*.min.css", "dist.min.css", clean());
+  rule("dist/*.min.css", "dist.min.css", concat());
   rule.each("css/*.css", fez.mapFile("dist/%f.min.css"), clean());
   rule.each("*.less", fez.mapFile("css/%f.css"), less({ rootpath: "public/" }));
 };
