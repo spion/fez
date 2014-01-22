bb4181d84e7d631ae016c97bd35851428396d9be
Isaac Wagner <isaacbwagner@gmail.com>
2014-01-21 20:29:18 -0500

    Added comments to examples

--- examples/jshint/fez.js
+++ examples/jshint/fez.js
@@ -2,6 +2,12 @@ var fez = require("../../src/main"),
     jshint = require("fez-jshint");
 
 exports.lint = function(rule) {
+  /* 
+   * Define a rule foro each JavaSript file in the src/ directory. We will run
+   * JSHINT once for each source file, and we will not create a hidden output
+   * file, which means that it will run every time, regardless of whether inputs
+   * have changed.
+   */
   rule.each("src/*.js", jshint({
     curly: true,
     indent: 2
--- examples/less/fez.js
+++ examples/less/fez.js
@@ -4,8 +4,25 @@ var fez = require("../../src/main.js"),
     concat = require("fez-concat");
 
 exports.build = function(rule) {
+  /*
+   * Concat every minified css file into a single distribution. This is
+   * intentionally out of order to illustrate that rule declaration order
+   * doesn't matter in Fez.
+   */  
   rule("dist/*.min.css", "dist.min.css", concat());
+
+  /*
+   * Run cleancss on each css file in the css/ directory, producing a
+   * corresponding minified file for each input in the dist/ directory. mapFile,
+   * as it's used here, names the output file base on the input, where %f is
+   * replaced with the filename of the input minus its extension.
+   */
   rule.each("css/*.css", fez.mapFile("dist/%f.min.css"), clean());
+
+  /*
+   * Run LESS on every less source file, producing a corresponding css file in
+   * the css directory. Pass some configuration options to LESS.
+   */
   rule.each("*.less", fez.mapFile("css/%f.css"), less({ rootpath: "public/" }));
 };
 
